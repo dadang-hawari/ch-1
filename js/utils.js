541808d013f131854dc0621b9ead729cf9c41612
dadanghawari <muhammaddadanghawari@gmail.com>
2024-02-26 21:47:20 +0800

    first commit

---
+++ js/utils.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'primary': '#F1F3FF',
+          'blue': '#0D28A6',
+          'gray-custom': '#c4c4c4',
+          'gray-custom-bg': '#CFD4ED'
+        },
+      },
+    }
+  }
